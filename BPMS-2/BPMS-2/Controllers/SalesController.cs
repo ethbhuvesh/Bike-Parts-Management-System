e400d63396094a92931119608fea401520e2c83a
Bhuvesh Gupta <bgupta1@umd.edu>
2022-11-10 09:46:19 -0500

    Customer View Added

--- BPMS-2/BPMS-2/Controllers/SalesController.cs
+++ BPMS-2/BPMS-2/Controllers/SalesController.cs
@@ -1,5 +1,6 @@
 ﻿using BPMS_2.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,16 +10,39 @@ namespace BPMS_2.Controllers
     {
 
         private readonly BPMS_2Context _context;
-
-        public SalesController(BPMS_2Context context)
+        private readonly UserManager<IdentityUser> _userManager;
+        public SalesController(BPMS_2Context context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
+
+
+
+        public async Task<string> GetCurrentUserId()
+        {
+            IdentityUser usr = await GetCurrentUserAsync();
+            return usr?.UserName;
+        }
+
+        private Task<IdentityUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
 
         [Authorize(Roles="Admin")]
         public async Task<IActionResult> Index()
         {
             return View(await _context.CartModel.ToListAsync());
+        }
+
+
+        public async Task<IActionResult> UserRecord()
+        {
+            var UID = await GetCurrentUserId();
+            var records=from record in _context.CartModel
+                      where record.UID == UID
+                      select record;
+
+            return View(records);
         }
     }
 }
